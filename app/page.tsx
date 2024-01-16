a9ad2a82c93d9cb222caeadfb631961ca330261b
benji tembwa <benjitembwa7@gmail.com>
2024-01-16 12:27:57 +0100

    secont-commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='h1-bold'>Next js 14 I&apos;m comming</h1>
+      <h1 className='h2-bold'>Next js 14 I&apos;m comming</h1>
+      <h1 className='h3-bold'>Next js 14 I&apos;m comming</h1>
+    </div>
+  )
+}
