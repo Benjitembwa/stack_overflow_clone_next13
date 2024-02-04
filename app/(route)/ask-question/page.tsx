8d8fcf9c24b52588fed32ec5483dabe2e5efee1b
benji tembwa <benjitembwa7@gmail.com>
2024-02-04 18:34:38 +0100

    create the ask question page

--- app/(route)/ask-question/page.tsx
+++ app/(route)/ask-question/page.tsx
@@ -1,7 +1,14 @@
-const page = () => {
+import Question from "@/components/forms/Question";
+
+const Page = () => {
   return (
-    <div>Ask question</div>
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>      
+    </div>
   )
 }
 
-export default page;
+export default Page;
---
+++ lib/validations.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const QuestionSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title must be at least 2 characters.",
+  }).max(130, {
+    message: "Title can't go above 130 characters"
+  }),
+  explanation: z.string().min(100, {
+    message: "Title must be at least 100 characters.",
+  }),
+  tags : z.array(z.string().min(1, {
+    message: "Title must be at least 2 characters.",
+  }).max(15, {
+    message: "can't go above 15 characters"
+  })).min(2, {
+    message: "At least 3 tags.",
+  }).max(3, {
+    message: "can't go above 3 Tags"
+  })
+});
